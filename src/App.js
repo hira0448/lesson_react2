10285a1af2b85bac37afb05eebe76edfece195e8
hira0448 <99321564+hira0448@users.noreply.github.com>
2022-05-03 16:59:58 +0900

    レンダリングの最適化2(useCallback) アロー関数で定義した関数は毎回、新しい関数が生成されたものと定義されるのでuseCallbackでメモ化する

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
@@ -10,9 +10,9 @@ export default function App() {
     setText(e.target.value);
   };
 
-  const onClickOpen = () => {
-    setOpen(!open);
-  };
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
 
   return (
     <div className="App">
@@ -20,7 +20,7 @@ export default function App() {
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -7,19 +7,20 @@ const style = {
 };
 
 export const ChildArea = memo((props) => {
-  const { open } = props;
+  const { open, onClickClose } = props;
   console.log("ChildAreaがレンダリングされた！");
 
-  const data = [...Array(2000).keys()];
-  data.forEach(() => {
-    console.log(data);
-  });
+  // const data = [...Array(2000).keys()];
+  // data.forEach(() => {
+  //   console.log(data);
+  // });
 
   return (
     <>
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
